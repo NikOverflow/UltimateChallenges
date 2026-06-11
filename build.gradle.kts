8eacdb05a27ddbcce12e86c73d8bd4ebaed73208
NikOverflow <nikoverflow007@gmail.com>
2026-06-11 18:56:17 +0200

    feat: add modrinth publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("com.modrinth.minotaur") version "2.9.0"
 }
 
 group = "com.nikoverflow"
@@ -31,4 +32,16 @@ tasks {
             )
         }
     }
+}
+
+modrinth {
+    token.set(project.findProperty("modrinth_token")?.toString() ?: "")
+    projectId.set("ultimate-challenges")
+    versionType.set("release")
+    versionName.set("UltimateChallenges ${project.property("plugin_version").toString()}")
+    uploadFile.set(tasks.jar)
+    gameVersions.addAll(project.property("minecraft_version").toString())
+    loaders.add("paper")
+    syncBodyFrom.set(rootProject.file("README.md").readText(Charsets.UTF_8))
+    changelog.set(rootProject.file("CHANGELOG.md").readText(Charsets.UTF_8))
 }
